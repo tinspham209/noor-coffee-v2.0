eb33ca132214442ec9f4fc1b476d098e154f2374
tinspham209 <tindeptrai1@gmail.com>
2020-12-18 14:25:58 +0700

    setup route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Footer, Navbar, NotFound, Spinner } from "./components";
+import ScrollToTop from "./utils";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import GlobalStyle from "./GlobalStyle";
+const Home = React.lazy(() => import("./pages/Home/Home"));
+const Blog = React.lazy(() => import("./pages/Blog/Blog"));
+
 function App() {
 	return (
-		<div className="">
-			<h2>hello world</h2>
-		</div>
+		<Suspense fallback={<Spinner />}>
+			<Router>
+				<CssBaseline />
+				<GlobalStyle />
+				<ScrollToTop />
+				<Navbar />
+				<Switch>
+					<Route path="/" exact component={Home} />
+					<Route path="/blog" exact component={Blog} />
+					<Route component={NotFound} />
+				</Switch>
+				<Footer />
+			</Router>
+		</Suspense>
 	);
 }
 
