202aeb9a6eb27137ecf91fa34509589853c5e9aa
tinspham209 <tindeptrai1@gmail.com>
2020-12-18 14:25:03 +0700

    export component

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Slider } from "./Slider/Slider";
+export { default as InfoSection } from "./InfoSection/InfoSection";
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as Footer } from "./Footer/Footer";
+export { default as NotFound } from "./UI/NotFound";
+export { default as Spinner } from "./UI/Spinner";
