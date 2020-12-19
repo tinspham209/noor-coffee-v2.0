da1f0e94a535c617903d8c541786e7589d6c52ea
tinspham209 <tindeptrai1@gmail.com>
2020-12-20 00:23:28 +0700

    update: InfoSection About

---
+++ src/components/InfoSection/InfoSection.elements.js
@@ -0,0 +1,112 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+	info: {
+		color: "black",
+		padding: "80px 0",
+		background: "#f5f5f5",
+	},
+	spinner: {
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	infoRow: {
+		alignItems: "center",
+
+		display: "flex",
+		flexWrap: "wrap",
+	},
+
+	infoColumn: {
+		maxWidth: "50%",
+		marginBottom: "15px",
+		padding: "0 15px",
+
+		flex: "1",
+		flexBasis: "50%",
+
+		[theme.breakpoints.down("md")]: {
+			maxWidth: "100%",
+
+			display: "flex",
+			justifyContent: "center",
+			flexBasis: "100%",
+		},
+	},
+
+	imgWrapper: {
+		maxWidth: "555px",
+		display: "flex",
+		justifyContent: "flex-start",
+		position: "relative",
+	},
+
+	img: {
+		paddingRight: 0,
+		border: 0,
+
+		maxWidth: "100%",
+		maxHeight: "500px",
+
+		display: "inline-block",
+		verticalAlign: "middle",
+	},
+
+	imgAbout: {
+		position: "absolute",
+		bottom: "-20px",
+		left: 0,
+
+		padding: "10px",
+		backgroundColor: "rgba(0,0,0,0.5)",
+
+		borderBottomRightRadius: "20px",
+		MozBorderRadiusBottomright: "20px",
+		WebkitBorderBottomRightRadius: "20px",
+
+		borderTopRightRadius: "20px",
+		MozBorderRadiusTopright: "20px",
+		WebkitBorderTopRightRadius: "20px",
+
+		color: "white",
+
+		[theme.breakpoints.down("md")]: {
+			"& h3": {
+				fontSize: "40px",
+			},
+		},
+	},
+
+	textWrapper: {
+		height: "auto",
+		display: "grid",
+		gridGap: "4rem",
+		gridTemplateColumns: "repeat(1, 1fr)",
+
+		[theme.breakpoints.down("md")]: {
+			marginTop: "50px",
+			gridGap: "3rem",
+		},
+	},
+	text: {
+		color: "#ea8025",
+		fontWeight: 600,
+	},
+	subTitle: {},
+	button: {
+		borderRadius: "20px",
+		fontSize: "20px",
+		background: "#f5a561",
+		color: "white",
+		fontWeight: "bold",
+
+		"&:hover": {
+			background: "rgba(245,165,97,0.9)",
+		},
+
+		[theme.breakpoints.down("md")]: {
+			width: "100%",
+		},
+	},
+}));
--- src/components/InfoSection/InfoSection.jsx
+++ src/components/InfoSection/InfoSection.jsx
@@ -1,9 +1,61 @@
+import {
+	Button,
+	CircularProgress,
+	Container,
+	Typography,
+} from "@material-ui/core";
 import React from "react";
+import { useStyles } from "./InfoSection.elements";
+import { Link } from "react-router-dom";
+import BlockContent from "@sanity/block-content-to-react";
 
-const InfoSection = () => {
+const InfoSection = ({ data }) => {
+	console.log("data: ", data);
+
+	const classes = useStyles();
 	return (
-		<div>
-			<h2>InfoSection</h2>
+		<div className={classes.info}>
+			{data ? (
+				<Container maxWidth="lg">
+					<div className={classes.infoRow}>
+						<div className={classes.infoColumn}>
+							<div className={classes.imgWrapper}>
+								<img
+									src={data.mainImage.asset.url}
+									alt="imgWrapper"
+									className={classes.img}
+								/>
+								<div className={classes.imgAbout}>
+									<Typography variant="h3">ABOUT NOOR.</Typography>
+								</div>
+							</div>
+						</div>
+						<div className={classes.infoColumn}>
+							<div className={classes.textWrapper}>
+								<Typography variant="h4" className={classes.text}>
+									{data.title}
+								</Typography>
+								<div>
+									<BlockContent
+										blocks={data.body}
+										projectId="1nauekv7"
+										dataset="production"
+									/>
+								</div>
+								<Link to="/about">
+									<Button variant="contained" className={classes.button}>
+										Về chúng tôi
+									</Button>
+								</Link>
+							</div>
+						</div>
+					</div>
+				</Container>
+			) : (
+				<div className={classes.spinner}>
+					<CircularProgress />
+				</div>
+			)}
 		</div>
 	);
 };
