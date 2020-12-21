2dc732aecdd369dcf0a49296d058635cb46dcf19
tinspham209 <tindeptrai1@gmail.com>
2020-12-21 14:23:35 +0700

    remove createMuiTheme

--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./app/store";
-import { MuiThemeProvider } from "@material-ui/core";
-import { THEME } from "./GlobalStyle";
 
 ReactDOM.render(
 	<React.Fragment>
 		<Provider store={store}>
-			<MuiThemeProvider theme={THEME}>
-				<App />
-			</MuiThemeProvider>
+			<App />
 		</Provider>
 	</React.Fragment>,
 	document.getElementById("root")
