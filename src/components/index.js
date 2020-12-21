646ca53b61a50e7b7014d374c0699543ba08ebb6
tinspham209 <tindeptrai1@gmail.com>
2020-12-21 11:31:39 +0700

    export component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as FullPage } from "./FullPage/FullPage";
 export { default as InfoSection } from "./InfoSection/InfoSection";
+export { default as HighlightPosts } from "./HighlightPosts/HighlightPosts";
 export { default as InfoMaps } from "./InfoMaps/InfoMaps";
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Footer } from "./Footer/Footer";
