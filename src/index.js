3b4c98411627c45693ce9f383af65711525e6605
tinspham209 <tindeptrai1@gmail.com>
2020-12-21 11:32:12 +0700

    update: fontFamity Global

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./app/store";
+import { MuiThemeProvider } from "@material-ui/core";
+import { THEME } from "./GlobalStyle";
 
 ReactDOM.render(
 	<React.Fragment>
 		<Provider store={store}>
-			<App />
+			<MuiThemeProvider theme={THEME}>
+				<App />
+			</MuiThemeProvider>
 		</Provider>
 	</React.Fragment>,
 	document.getElementById("root")
