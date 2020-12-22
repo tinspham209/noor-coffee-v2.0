34e0836752e64fa5a223e950cf021e1bbe7a0f4b
tinspham209 <tindeptrai1@gmail.com>
2020-12-23 02:41:37 +0700

    update: Hero page

--- src/components/FullPage/FullPage.elements.js
+++ src/components/FullPage/FullPage.elements.js
@@ -1,33 +1,163 @@
 import { makeStyles } from "@material-ui/core/styles";
-import background from "../../assets/img/anh-bia.webp";
-import backgroundMobile from "../../assets/img/background-mobile.webp";
 
 export const useStyles = makeStyles((theme) => ({
+	hero: {
+		height: "calc(100vh - 80px)",
+		position: "relative",
+		overflow: "hidden",
+		backgroundColor: "#cab9a9",
+
+		[theme.breakpoints.down("md")]: {
+			height: "calc(100vh - 80px - 60px)",
+		},
+	},
+
+	heroWrapper: {
+		width: "100%",
+		height: "100%",
+
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "center",
+
+		overflow: "hidden",
+		position: "relative",
+	},
+
+	heroSlide: {
+		zIndex: "9",
+		width: "100%",
+		height: "100%",
+
+		transition: "all 1 ease-in-out",
+	},
+	heroSlider: {
+		position: "absolute",
+		top: 0,
+		left: 0,
+		width: "100%",
+		height: "100%",
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+
+		"&::before": {
+			content: '""',
+			position: "absolute",
+			zIndex: "10",
+			width: "100%",
+			height: "100vh",
+			bottom: "0vh",
+			overFlow: "hidden",
+			opacity: "0.7",
+			background:
+				"linear-gradient(0deg, rgba(0,0,0,0.2) 0%, rgba(0,0,0,0.2) 50%, rgba(0,0,0,0.2) 100%)",
+		},
+	},
+	heroImage: {
+		position: "absolute",
+		top: 0,
+		left: 0,
+		width: "100vw",
+		height: "100vh",
+		objectFit: "cover",
+	},
+	heroContent: {
+		position: "relative",
+		zIndex: "10",
+
+		display: "flex",
+		flexDirection: "column",
+		justifyContent: "center",
+		alignItems: "center",
+
+		color: "#fff",
+	},
+	heroContentTitle: {
+		fontSize: "clamp(7.3rem, 20vw, 20rem)",
+		textShadow: "-8px 7px rgba(0,0,0,0.7)",
+		fontWeight: "500",
+		textTransform: "uppercase",
+		fontFamily: '"BebasNeue","Lato", san-serif',
+	},
+	heroContentSubTitle: {
+		fontSize: "56px",
+		textTransform: "uppercase",
+		fontFamily: '"BebasNeue","Lato", san-serif',
+
+		[theme.breakpoints.down("md")]: {
+			fontSize: "32px",
+		},
+	},
+	sliderButton: {
+		position: "absolute",
+		bottom: "50px",
+		right: "50px",
+		display: "flex",
+		zIndex: "99",
+
+		[theme.breakpoints.down("md")]: {
+			bottom: "10%",
+			right: "2%",
+		},
+	},
+	arrowButtons: {
+		width: "50px",
+		height: "50px",
+
+		color: "black",
+		backgroundColor: "#f5f5f5",
+		borderRadius: "50px",
+		border: "none",
+		outline: "none",
+
+		cursor: "pointer",
+
+		padding: "10px",
+		marginRight: "10px",
+		userSelect: "none",
+		transition: "all 0.2s ease-in-out",
+
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+
+		"&:hover": {
+			background: "rgba(246,194,73, 1)",
+			transform: "scale(1.05)",
+		},
+		"& svg": {
+			fontSize: "24px",
+		},
+	},
+
 	slider: {
 		width: "100%",
 		height: "calc( 100vh - 80px)",
 		display: "flex",
 		justifyContent: "center",
 		alignItems: "center",
-		background: `url(${background}) center center/cover no-repeat`,
 		backgroundColor: "#cab9a9",
 
-		[theme.breakpoints.down("md")]: {
-			background: `url(${backgroundMobile}) center center/cover no-repeat`,
-		},
+		[theme.breakpoints.down("md")]: {},
 	},
+
 	fixedScrollDown: {
 		position: "absolute",
 		bottom: "1%",
 		left: "50%",
+		zIndex: "99",
+
 		display: "flex",
 		justifyContent: "center",
 		flexDirection: "column",
 		alignItems: "center",
+
 		color: "white",
 
 		[theme.breakpoints.down("md")]: {
-			left: "40%",
+			bottom: "10%",
+			left: "35%",
 		},
 	},
 	icon: {
