b8a6dc9163751421caa5f380a677d693abb5831f
tinspham209 <tindeptrai1@gmail.com>
2020-12-23 01:18:18 +0700

    add: estimate Reading Time

--- src/utils/index.js
+++ src/utils/index.js
@@ -9,3 +9,19 @@ export default function ScrollToTop() {
 	}, [pathname]);
 	return null;
 }
+
+export const estimateReadingTime = (string) => {
+	const wordsPerMinute = 200;
+	let result;
+
+	let stringMergeArray = [];
+	string.map((item, index) => stringMergeArray.push(item.children[0].text));
+	const stringMerge = stringMergeArray.join("");
+
+	let textLength = stringMerge.split(" ").length;
+	if (textLength > 0) {
+		let value = Math.ceil(textLength / wordsPerMinute);
+		result = `~${value} min read`;
+	}
+	return result;
+};
