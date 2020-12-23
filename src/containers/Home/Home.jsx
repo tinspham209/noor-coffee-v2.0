79ea9c7dda709ac99d13f32d0df7de5b3e7d2f69
tinspham209 <tindeptrai1@gmail.com>
2020-12-24 01:38:17 +0700

    add: Products

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -5,6 +5,7 @@ import {
 	InfoMaps,
 	InfoSection,
 	InstaPosts,
+	Products,
 	SpinnerBook,
 } from "../../components";
 import {
@@ -12,14 +13,31 @@ import {
 	setHero,
 	setInstaPosts,
 	setPosts,
+	setProductCoffee,
+	setProductColdbrew,
+	setProductJuice,
+	setProductMacchiato,
+	setProductSpecial,
+	setProductTea,
+	setProductYogurt,
 } from "../../app/slice/fetchApi";
 import { useDispatch } from "react-redux";
+import { LazyLoadComponent } from "react-lazy-load-image-component";
 import {
 	fetchAboutData,
 	fetchBlogData,
 	fetchHeroData,
 	fetchInstaPost,
+	fetchProduct,
 } from "../../api";
+import {
+	COFFEE,
+	COLDBREW,
+	JUICE,
+	MACCHIATO,
+	TEA,
+	YOGURT,
+} from "../../app/ItemTypes";
 
 const Home = () => {
 	const [spinner, setSpinner] = useState(true);
@@ -79,9 +97,53 @@ const Home = () => {
 	}, [dispatch]);
 
 	useEffect(() => {
+		fetchProduct()
+			.then((products) => {
+				console.log("products: ", products);
+				const special = [];
+
+				const coffee = [];
+				const coldbrew = [];
+				const tea = [];
+				const macchiato = [];
+				const yogurt = [];
+				const juice = [];
+
+				products.map((product, index) => {
+					if (product.projectType === COFFEE) coffee.push(product);
+					else if (product.projectType === COLDBREW) coldbrew.push(product);
+					else if (product.projectType === TEA) tea.push(product);
+					else if (product.projectType === MACCHIATO) macchiato.push(product);
+					else if (product.projectType === YOGURT) yogurt.push(product);
+					else if (product.projectType === JUICE) juice.push(product);
+
+					product.special && special.push(product);
+
+					return null;
+				});
+				let action;
+				action = setProductCoffee(coffee);
+				dispatch(action);
+				action = setProductColdbrew(coldbrew);
+				dispatch(action);
+				action = setProductTea(tea);
+				dispatch(action);
+				action = setProductMacchiato(macchiato);
+				dispatch(action);
+				action = setProductYogurt(yogurt);
+				dispatch(action);
+				action = setProductJuice(juice);
+				dispatch(action);
+				action = setProductSpecial(special);
+				dispatch(action);
+			})
+			.catch((error) => console.log("error", error));
+	}, [dispatch]);
+
+	useEffect(() => {
 		setTimeout(() => {
 			setSpinner(false);
-		}, 3000);
+		}, 500);
 	}, []);
 
 	return (
@@ -91,10 +153,21 @@ const Home = () => {
 			) : (
 				<>
 					<FullPage />
-					<InfoSection />
-					<InfoMaps />
-					<HighlightPosts />
-					<InstaPosts />
+					<LazyLoadComponent>
+						<InfoSection />
+					</LazyLoadComponent>
+					<LazyLoadComponent>
+						<InfoMaps />
+					</LazyLoadComponent>
+					<LazyLoadComponent>
+						<HighlightPosts />
+					</LazyLoadComponent>
+					<LazyLoadComponent>
+						<InstaPosts />
+					</LazyLoadComponent>
+					<LazyLoadComponent>
+						<Products />
+					</LazyLoadComponent>
 				</>
 			)}
 		</div>
