b92341d2733b347434a2153b09b3fe2abee4d0c5
tinspham209 <tindeptrai1@gmail.com>
2020-12-27 23:56:09 +0700

    add: heroProducts page

--- noorcoffeev20/schemas/schema.js
+++ noorcoffeev20/schemas/schema.js
@@ -12,6 +12,7 @@ import author from "./author";
 import product from "./product";
 import insta from "./insta";
 import hero from "./hero";
+import heroProduct from "./heroProduct";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -28,6 +29,7 @@ export default createSchema({
 		product,
 		insta,
 		hero,
+		heroProduct,
 		// When added to this list, object types can be used as
 		// { type: 'typename' } in other document schemas
 		blockContent,
