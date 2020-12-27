929fe2e728ba71415c3fd7d7e5d0e9a1869dd40a
tinspham209 <tindeptrai1@gmail.com>
2020-12-27 23:45:31 +0700

    add: list products

---
+++ src/data/index.js
@@ -0,0 +1,30 @@
+export const products = [
+	{
+		id: "special",
+		name: "nổi bật",
+	},
+	{
+		id: "coffee",
+		name: "cà phê",
+	},
+	{
+		id: "coldbrew",
+		name: "coldbrew",
+	},
+	{
+		id: "tea",
+		name: "trà",
+	},
+	{
+		id: "macchiato",
+		name: "macchiato",
+	},
+	{
+		id: "yogurt",
+		name: "sữa chua",
+	},
+	{
+		id: "juice",
+		name: "nước trái cây",
+	},
+];
