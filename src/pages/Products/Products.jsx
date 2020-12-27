8605107a03fe26e888279e789439eb57aee96312
tinspham209 <tindeptrai1@gmail.com>
2020-12-27 23:45:42 +0700

    add: products page

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect } from "react";
-
+import ProductsContainer from "../../containers/Products/Products";
 const Products = () => {
 	useEffect(() => {
 		document.title = "Menu - NOOR - Coffee & Tea - Coworking Cafe";
 	}, []);
 
-	return <div>Menu</div>;
+	return (
+		<>
+			<ProductsContainer />
+		</>
+	);
 };
 
 export default Products;
