c15ec2bd6230c23345666cc7d3e0e9b9e71ea03b
tinspham209 <tindeptrai1@gmail.com>
2020-12-28 00:02:11 +0700

    update: fontsize of title

--- src/components/Products/Products.elements.js
+++ src/components/Products/Products.elements.js
@@ -162,7 +162,7 @@ export const useStyles = makeStyles((theme) => ({
 		WebkitLineClamp: "2",
 
 		[theme.breakpoints.down("md")]: {
-			fontSize: "17px",
+			fontSize: "16px",
 		},
 	},
 	price: {
