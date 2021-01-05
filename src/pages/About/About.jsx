55b60e442a0014956c6487549c41157c0db2bf50
tinspham209 <tindeptrai1@gmail.com>
2021-01-05 22:03:38 +0700

    add: About page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,30 +1,11 @@
 import React from "react";
-import { SectionLink } from "../../components";
-import { AiOutlineHome } from "react-icons/ai";
-import Img from "../../assets/img/developing.svg";
-const feedback = {
-	subHeader: "NOOR Coffee & Tea",
-	header: "This feature is developing",
-	description: "You can use it as soon as it possible",
-	linkBtn: "/",
-	contentBtn: "Trang chủ",
-	img: Img,
-	icon: <AiOutlineHome />,
-};
+import AboutContainer from "../../containers/About/About";
 
 const About = () => {
 	return (
-		<div>
-			<SectionLink
-				subHeader={feedback.subHeader}
-				header={feedback.header}
-				description={feedback.description}
-				linkBtn={feedback.linkBtn}
-				img={feedback.img}
-				contentBtn={feedback.contentBtn}
-				iconBtn={feedback.icon}
-			/>
-		</div>
+		<>
+			<AboutContainer />
+		</>
 	);
 };
 
