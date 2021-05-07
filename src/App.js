fef0c9c6a844595b0711f204bbd723d2b4f12dc7
tinspham209 <tindeptrai1@gmail.com>
2021-05-07 12:52:46 +0700

    build(stores): Add new store and redirect link

--- src/App.js
+++ src/App.js
@@ -11,37 +11,43 @@ const Stores = React.lazy(() => import("./pages/Stores/Stores"));
 const Feedback = React.lazy(() => import("./pages/Feedback/Feedback"));
 const Membership = React.lazy(() => import("./pages/Membership/Membership"));
 const About = React.lazy(() => import("./pages/About/About"));
+const Loship = React.lazy(() => import("./pages/Loship"));
+const Now = React.lazy(() => import("./pages/Now"));
+const Grab = React.lazy(() => import("./pages/Grab"));
 const SinglePost = React.lazy(() =>
-	import("./containers/SinglePost/SinglePost")
+  import("./containers/SinglePost/SinglePost")
 );
 const SingleProduct = React.lazy(() =>
-	import("./containers/SingleProduct/SingleProduct")
+  import("./containers/SingleProduct/SingleProduct")
 );
 
 function App() {
-	return (
-		<Suspense fallback={<Spinner />}>
-			<Router>
-				<CssBaseline />
-				<GlobalStyle />
-				<ScrollToTop />
-				<Navbar />
-				<Switch>
-					<Route path="/" exact component={Home} />
-					<Route path="/blog" exact component={Blog} />
-					<Route path="/blog/:slug" exact component={SinglePost} />
-					<Route path="/products" exact component={Products} />
-					<Route path="/products/:slug" exact component={SingleProduct} />
-					<Route path="/stores" exact component={Stores} />
-					<Route path="/feedback" exact component={Feedback} />
-					<Route path="/membership" exact component={Membership} />
-					<Route path="/about" exact component={About} />
-					<Route component={NotFound} />
-				</Switch>
-				<Footer />
-			</Router>
-		</Suspense>
-	);
+  return (
+    <Suspense fallback={<Spinner />}>
+      <Router>
+        <CssBaseline />
+        <GlobalStyle />
+        <ScrollToTop />
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/blog" exact component={Blog} />
+          <Route path="/blog/:slug" exact component={SinglePost} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/products/:slug" exact component={SingleProduct} />
+          <Route path="/stores" exact component={Stores} />
+          <Route path="/feedback" exact component={Feedback} />
+          <Route path="/membership" exact component={Membership} />
+          <Route path="/about" exact component={About} />
+          <Route path="/now" exact component={Now} />
+          <Route path="/grab" exact component={Grab} />
+          <Route path="/loship" exact component={Loship} />
+          <Route component={NotFound} />
+        </Switch>
+        <Footer />
+      </Router>
+    </Suspense>
+  );
 }
 
 export default App;
--- src/pages/Stores/Stores.jsx
+++ src/pages/Stores/Stores.jsx
@@ -1,64 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { InfoMaps, SpinnerBook, Section } from "../../components";
-import grabImg from "../../assets/img/grab.png";
-import loshipImg from "../../assets/img/loship.png";
+
+import { online } from "../../data";
 const Stores = () => {
-	const [spinner, setSpinner] = useState(true);
-	useEffect(() => {
-		document.title = "Cửa hàng - NOOR - Coffee & Tea - Coworking Cafe";
-	}, []);
+  const [spinner, setSpinner] = useState(true);
+  useEffect(() => {
+    document.title = "Cửa hàng - NOOR - Coffee & Tea - Coworking Cafe";
+  }, []);
 
-	useEffect(() => {
-		setTimeout(() => {
-			setSpinner(false);
-		}, 200);
-	}, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setSpinner(false);
+    }, 200);
+  }, []);
 
-	const grab = {
-		subHeader: "NOOR Coffee & Tea",
-		header: "Grab Food",
-		description:
-			'Đặt đồ uống trực tuyến và Grab mang đến tận nơi cho bạn với dịch vụ giao hàng "thần tốc"',
-		linkBtn:
-			"https://food.grab.com/vn/vi/restaurant/noor-coffee-tea-delivery/5-CZJVRNAAL6TDJJ",
-		img: grabImg,
-	};
-
-	const loship = {
-		subHeader: "NOOR Coffee & Tea",
-		header: "Loship",
-		description:
-			'Đặt đồ uống trực tuyến và Loship mang đến tận nơi cho bạn với dịch vụ giao hàng "thần tốc"',
-		linkBtn: "https://loship.vn/nocaphe",
-		img: loshipImg,
-	};
-
-	return (
-		<>
-			{spinner ? (
-				<SpinnerBook />
-			) : (
-				<>
-					<InfoMaps />
-					<Section
-						subHeader={grab.subHeader}
-						header={grab.header}
-						description={grab.description}
-						linkBtn={grab.linkBtn}
-						img={grab.img}
-					/>
-					<Section
-						subHeader={loship.subHeader}
-						header={loship.header}
-						description={loship.description}
-						linkBtn={loship.linkBtn}
-						img={loship.img}
-						dark
-					/>
-				</>
-			)}
-		</>
-	);
+  return (
+    <>
+      {spinner ? (
+        <SpinnerBook />
+      ) : (
+        <>
+          <InfoMaps />
+          {online.map((store, index) => (
+            <Section
+              subHeader={store.subHeader}
+              header={store.header}
+              description={store.description}
+              linkBtn={store.linkBtn}
+              img={store.img}
+              key={index}
+              dark={index === "1" ? true : false}
+            />
+          ))}
+        </>
+      )}
+    </>
+  );
 };
 
 export default Stores;
